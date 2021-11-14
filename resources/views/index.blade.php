556066c66732be176486f340bcaa9d34068198b0
ridjalfathoni <fathonirosyidi@gmail.com>
2021-11-14 09:20:59 +0000

    [add] bootstrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
+  </head>
+  <body>
+      <nav class="nav justify-content-center">
+        <a class="nav-link active" href="#">Active link</a>
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link disabled" href="#">Disabled link</a>
+      </nav>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="{{ mix('js/app.js') }}"></script>
+  </body>
+</html>
